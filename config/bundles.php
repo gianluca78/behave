e09202e94578c30df601de21023adf8aac2cc97e
gianluca78 <gianlucamerlo@yahoo.it>
2018-01-04 12:51:50 +0100

    added security

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Merlo\Bundle\MerloCrudBundle\MerloCrudBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
