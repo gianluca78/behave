49651357b7e43922e8ba882ef832e700f0e14cc1
gianluca78 <gianlucamerlo@yahoo.it>
2018-05-29 11:47:34 +0200

    bug fix

--- src/Validator/Constraints/IsMultipleValidator.php
+++ src/Validator/Constraints/IsMultipleValidator.php
@@ -11,7 +11,7 @@ class IsMultipleValidator extends ConstraintValidator {
         $observationLengthInSeconds = $directObservationItem->getObservationLengthInMinutes() * 60;
 
         if ($directObservationItem->getIntervalLengthInSeconds() &&
-            $directObservationItem->getIntervalLengthInSeconds() % $observationLengthInSeconds != 0) {
+            $observationLengthInSeconds % $directObservationItem->getIntervalLengthInSeconds() != 0) {
             $this->context->buildViolation($constraint->message)
                 ->atPath('intervalLengthInSeconds')
                 ->setParameter('{{ string }}', $directObservationItem->getIntervalLengthInSeconds())
