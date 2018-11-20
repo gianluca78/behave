42ed2547906a876cce7ff1c96ce89703ee582a0e
gianluca78 <gianlucamerlo@yahoo.it>
2018-11-20 16:41:18 +0100

    close issue #8

--- src/Entity/ObservationPhase.php
+++ src/Entity/ObservationPhase.php
@@ -3,9 +3,11 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ObservationPhaseRepository")
+ * @UniqueEntity({"name", "observation"})
  */
 class ObservationPhase
 {
